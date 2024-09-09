dea706a94321a6e3452210e6f4d27de7e71fec0c
Albin J. Tharayil <44770831+nerdyalbs007@users.noreply.github.com>
2024-09-09 22:47:50 +0530

    Added changes for getAllTracks

--- src/main/java/com/music/track/service/impl/TrackServiceImpl.java
+++ src/main/java/com/music/track/service/impl/TrackServiceImpl.java
@@ -4,18 +4,23 @@ import com.music.track.dto.TrackRequest;
 import com.music.track.model.Track;
 import com.music.track.service.TrackService;
 import org.springframework.stereotype.Service;
+import com.music.track.repository.TrackRepository;
 
 import java.util.List;
 
 @Service
 public class TrackServiceImpl implements TrackService {
+
+    @Autowired
+	private TrackRepository trackRepository;
+
     @Override
     public Track createTrack(TrackRequest trackRequest) {
         return null;
     }
     @Override
     public List<Track> getAllTracks() {
-        return null;
+        return trackRepository.findAll();
     }
 
     @Override
